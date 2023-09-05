b96191d9bc6fbe0bdb1ffcca44ff2b8eb4635884
lofflerA <anne.loffler@unityhealth.to>
2023-09-05 10:56:02 -0400

    Add lab and transfusion table names

--- R/utils.R
+++ R/utils.R
@@ -106,9 +106,13 @@ coerce_to_datatable <- function(data) {
 #'
 #' Currently, the function only supports a subset of table names (see below) and
 #' expects the relevant tables in all databases to only differ based on their
-#' suffix (e.g., "admdad" vs. "admdad_subset"). Specifically, the function
-#' currently uses `grepl("^tablename",drm_table)` to look for table names that
-#' *start with* the same name as specified in DRM (e.g., 'admdad').
+#' suffix (e.g., "admdad" vs. "admdad_subset"). Specifically, for most table
+#' names, the function uses `grepl("^tablename",drm_table)` to look for table
+#' names that *start with* the same name as specified in DRM (e.g., 'admdad').
+#' Exceptions are the "lab" and "transfusion" tables. Because there are other
+#' tables with similar names (e.g., "transfusion_mapping" table), the function
+#' specifically looks for tables called either "lab"/"transfusion" or
+#' "lab_subset"/"transfusion_subset" (for HPC datacuts).
 #'
 #' @param dbcon (`DBIConnection`)\cr
 #' A database connection to any GEMINI database.
@@ -122,7 +126,7 @@ coerce_to_datatable <- function(data) {
 #' - `"ipintervention"`
 #' - `"ipcmg"`
 #' - `"transfusion"`
-#' - `"lab`
+#' - `"lab"`
 #'
 #' Users need to specify the full DRM table name (e.g., `"admdad"` instead of
 #' `"adm"`) to avoid potential confusion with other tables.
@@ -160,9 +164,17 @@ find_db_tablename <- function(dbcon, drm_table, verbose = TRUE) {
          'admdad','ipdiagnosis','ipintervention', or 'ipcmg'")
   } else {
 
-    ## find any tables in current DB that start with name of DRM table
-    table_name <- unique(dbListTables(dbcon)[grepl(
-      paste0("^", drm_table), dbListTables(dbcon))])
+    ## for lab & transfusion table table:
+    # check for specific table names lab/lab_subset and transfusion/transfusion_subset
+    # (otherwise, lab/transfusion_mapping or other tables might be returned)
+    if (drm_table %in% c("lab","transfusion")) {
+      table_name <- unique(dbListTables(dbcon)[dbListTables(dbcon) %in% c(drm_table,paste0(drm_table,"subset"))])
+    } else { # for all other tables, do simple grepl search based on first characters
+      ## find any tables in current DB that start with name of DRM table
+      table_name <- unique(dbListTables(dbcon)[grepl(
+        paste0("^", drm_table), dbListTables(dbcon))])
+    }
+
   }
 
   ## Check returned value
@@ -177,7 +189,7 @@ find_db_tablename <- function(dbcon, drm_table, verbose = TRUE) {
 
   # error if more than 1 table found
   if (length(table_name) > 1){
-    stop(paste0("Multiple tables corresponding to '", drm_table, "' identified in database '", db_name,  "'.
+    stop(paste0("Multiple tables corresponding to '", drm_table, "' identified in database '", db_name,  ": ", paste0(table_name, collapse = ", "), ".
                  Please ensure that the searched table name results in a unique match."))
   }
 
