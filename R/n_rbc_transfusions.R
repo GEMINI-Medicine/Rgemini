9c3b75970bdb05208ecfde09ccb5108fd11026a1
lofflerA <anne.loffler@unityhealth.to>
2023-09-05 10:50:26 -0400

    Standardize count function names

--- R/n_rbc_transfusions.R
+++ R/n_rbc_transfusions.R
@@ -1,8 +1,9 @@
 #' @title
-#' Count the number Red Blood Cell Transfusions per encounter
+#' Count the number Red Blood Cell (RBC) Transfusions per encounter
 #'
 #' @description
-#' This function calculates appropriate transfusions and any transfusions.
+#' This function calculates the number of appropriate transfusions and total of
+#' all transfusions per encounter.
 #' The definition of an appropriate transfusion can be found on the sample
 #' [My Practice Report](https://www.hqontario.ca/Portals/0/documents/qi/practice-reports/general-medicine-sample-report.html#transfusions-qi).
 #' A transfusion is defined by procedure in the below codes:
@@ -10,22 +11,30 @@
 #' 2.  [4137859](https://athena.ohdsi.org/search-terms/terms/4137859)
 #' 3.  [4022173](https://athena.ohdsi.org/search-terms/terms/4022173)
 #'
-#' An appropriate transfusion also requires that the patients pre-transfusion hemoglobin levels are  below 80 g/L.
-#' The Athena definition of a hemoglobin test:
+#' An appropriate transfusion also requires that the patients pre-transfusion
+#' hemoglobin levels are  below 80 g/L. The Athena definition of a hemoglobin
+#' test:
 #'  [3000963](https://athena.ohdsi.org/search-terms/terms/3000963)
 #'
-#' @param con \cr
-#' Database connection to any GEMINI database
+#' Red blood cell transfusions with no hemoglobin measurement within 48 hours
+#' prior to the transfusion are excluded. These scenarios are rare, typically
+#' occurring in approximately 2% of blood transfusions in GEMINI data.
 #'
-#' @param ipadmdad (`data.table`)\cr
-#' `ipadmdad` table as defined in the
-#' [GEMINI Data Repository Dictionary](https://drive.google.com/uc?export=download&id=1iwrTz1YVz4GBPtaaS9tJtU0E9Bx1QSM5).
+#' @note
+#' Transfusion data from two hospitals with known data quality issues are
+#' automatically removed by this function.
 #'
-#' @return
-#' `data.table`
-#' It includes three columns. It includes genc_id,
-#' n_app_rbc_transfusion_derived, and n_rbc_transfusion_derived.
-#' It will include all rows from the ipadmdad parameter. Any encounter without a
+#' @param dbcon (`DBIConnection`)\cr
+#' A database connection to any GEMINI database.
+#' @param cohort (`data.frame` or `data.table`)
+#' Cohort table with all relevant encounters of interest where each row
+#' corresponds to a single encounter. Must contain the following columns:
+#' - `genc_id` (`integer`): GEMINI encounter ID
+#' - `hospital_num` (`integer`): Hospital number
+#' @return (`data.table`)\cr
+#' Table with three columns: `genc_id`, `n_app_rbc_transfusion_derived`, and
+#' `n_rbc_transfusion_derived`. All
+#' It will include all rows from the cohort parameter. Any encounter without a
 #' transfusion will get a 0.
 #'
 #' @import RPostgreSQL lubridate
@@ -33,42 +42,58 @@
 #' @export
 #'
 
-rbc_transfusions <-function(con,
-                            ipadmdad
-                            ) {
-
+n_rbc_transfusions <-function(dbcon,
+                            cohort) {
 
   cat("\n***Note:***
   The output of this function is based on manual mapping of RBC transfusions by a GEMINI Subject Matter Expert.
   Please carefully check mapping coverage for your cohort of interest, or contact the GEMINI team if you require additional support.\n")
 
-  ipadmdad <- coerce_to_datatable(ipadmdad)
+  cat("\nThis function may take a few minutes to run...\n\n")
+
+  cohort <- coerce_to_datatable(cohort)
+
+  ## Cohort table MUST contain hospital_num (not hospital_id) for exclusion below to work correctly
+  if ("hospital_num" %ni% names(cohort)) {
+    stop("Input table 'cohort' is missing variable 'hospital_num'.
+    Please refer to the function documentation for more details.")
+  }
+
+  ## If relevant: Show warning notifying user of hospital exclusion
+  if (any(c(105,106) %in% unique(cohort$hospital_num))) {
+    warning("Excluding hospitals with known transfusion data quality issues.
+            Please refer to the function documentation for more details.", immediate. = TRUE)
+
+  }
+  cohort_subset <- cohort[hospital_num %ni% c(105, 106)]
+
+  temp_d_glist <- cohort_subset[,.(genc_id)][["genc_id"]]
 
 
-  ipadmdad_subset <- ipadmdad[hospital_num %ni% c(105, 106)]
-
-  temp_d_glist <- ipadmdad_subset[,.(genc_id)][["genc_id"]]
+  # find table names for all relevant tables (admdad/lab/transfusion)
+  admdad_table <- find_db_tablename(dbcon, "admdad", verbose = FALSE)
+  lab_table <- find_db_tablename(dbcon, "lab", verbose = FALSE)
+  transfusion_table <- find_db_tablename(dbcon, "transfusion", verbose = FALSE)
 
   dbGetQuery(
-    con ,
+    dbcon ,
     paste0(
       "drop table if exists lab_bb;
     create temp table lab_bb as
     select genc_id,
     result_value,
-    collection_date_time,
-    hospital_id
-    from lab l
+    collection_date_time
+    from ", lab_table, " l
     where l.test_type_mapped_omop = '3000963' and
       genc_id in (",
       paste(temp_d_glist, collapse = ","),
       ")")
-    )
+  )
 
-  dbGetQuery(con,
+  dbGetQuery(dbcon,
              paste0("drop table if exists transfusion_temp;
              create temp table transfusion_temp as
-             select * from transfusion a
+             select * from ", transfusion_table, " a
              where blood_product_mapped_omop in ('4022173','4137859','4144461')
              and genc_id in (",
                     paste(temp_d_glist, collapse = ","),
@@ -76,7 +101,7 @@ rbc_transfusions <-function(con,
              )
 
   bb <- dbGetQuery(
-    con,"select t.genc_id,
+    dbcon,paste0("select t.genc_id,
     issue_date_time ,
     l.collection_date_time,
     a.admission_date_time,
@@ -84,10 +109,10 @@ rbc_transfusions <-function(con,
     from
     transfusion_temp t
     left join lab_bb l on t.genc_id = l.genc_id
-    left join admdad a on t.genc_id = a.genc_id
+    left join ", admdad_table, " a on t.genc_id = a.genc_id
     where
     t.issue_date_time >= a.admission_date_time and
-    t.issue_date_time <= a.discharge_date_time ;") %>%
+    t.issue_date_time <= a.discharge_date_time ;")) %>%
     data.table()
 
   bb_2 <- bb %>%
@@ -123,7 +148,7 @@ rbc_transfusions <-function(con,
   ntrans <- res[!is.na(as.numeric(pre.hgb)), .N, genc_id]
 
   ipadmdad_preghg80 <- merge(
-    ipadmdad_subset[,.(genc_id)],
+    cohort_subset[,.(genc_id)],
     prehgbless80[, .(genc_id, N.Trans.with.pre.HGBLess80P = N)],
     by = "genc_id",
     all.x = T)# check duplicated  genc_id
