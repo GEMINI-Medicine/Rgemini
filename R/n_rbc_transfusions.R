6e4ae53b972f6db18baa826c6f5b68337c71c1d9
lofflerA <anne.loffler@unityhealth.to>
2023-09-06 11:08:34 -0400

    Update roxygen

--- R/n_rbc_transfusions.R
+++ R/n_rbc_transfusions.R
@@ -20,22 +20,25 @@
 #' prior to the transfusion are excluded. These scenarios are rare, typically
 #' occurring in approximately 2% of blood transfusions in GEMINI data.
 #'
-#' @note
-#' Transfusion data from two hospitals with known data quality issues are
-#' automatically removed by this function.
-#'
 #' @param dbcon (`DBIConnection`)\cr
 #' A database connection to any GEMINI database.
+#'
 #' @param cohort (`data.frame` or `data.table`)
 #' Cohort table with all relevant encounters of interest where each row
 #' corresponds to a single encounter. Must contain the following columns:
 #' - `genc_id` (`integer`): GEMINI encounter ID
 #' - `hospital_num` (`integer`): Hospital number
+#'
 #' @return (`data.table`)\cr
-#' Table with three columns: `genc_id`, `n_app_rbc_transfusion_derived`, and
-#' `n_rbc_transfusion_derived`. All
-#' It will include all rows from the cohort parameter. Any encounter without a
-#' transfusion will get a 0.
+#' Table with three columns: `genc_id`, `n_app_rbc_transfusion_derived` (number
+#' of appropriate RBC transfusions), and `n_rbc_transfusion_derived` (total of
+#' all RBC transfusions). Encounters without any transfusion will get a 0.
+#'
+#' @note
+#' Transfusion data from two hospitals with known data quality issues are
+#' automatically removed by this function. Any `genc_ids` from those sites are
+#' not included in the returned output. When merging the output of this function
+#' with another table, those `genc_ids` should have a value of `NA`.
 #'
 #' @import RPostgreSQL lubridate
 #'
