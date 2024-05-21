5572ef35e74d28ddb7c2f5d5d10a6356ebda58f1
loffleraSMH <Anne.Loffler@unityhealth.to>
2024-05-21 12:13:28 -0400

    #76 add unit tests

---
+++ tests/testthat/test-convert_datetime.R
@@ -0,0 +1,78 @@
+test_that(
+  "date-times provided as character input are parsed correctly with format = 'ymd HM' (default)", {
+    
+    date_time <- c("2020-01-01 12:00", "2020-01-01 00:00", "2020-01-01 12:00:00",
+                   "2020-01-01 00:00:00", "2021-02-05", NA, "", " ")
+    
+    # this should return warnings due to missing/invalid entries
+    # warnings: 3 missing, 1 invalid, 1 invalid due to date-only
+    expect_warning(convert_datetime(date_time))
+    
+    # run it without warnings and then check outputs below
+    res <- suppressWarnings(convert_datetime(date_time)) 
+  
+    expect_equal(class(res), c("POSIXct", "POSIXt"))
+    expect_equal(res, as.POSIXct(c("2020-01-01 12:00:00", "2020-01-01 00:00:00",
+                                   NA, NA, NA, NA, NA, NA), tz = "UTC"))
+
+  }
+)
+
+test_that(
+  "date-times provided as character input can be parsed according to multiple formats", {
+    
+    date_time <- c("2020-01-01 12:00", "2020-01-01 00:10", "2020-01-01 12:00:30",
+                   "2020-01-01 13:12:20", "2021-02-05", "2023-12-05")
+    
+    # should not result in warning due to multiple acceptable formats
+    expect_no_warning(convert_datetime(date_time, format = c("ymd HM", "ymd HMS", "ymd")))
+    res <- convert_datetime(date_time, format = c("ymd HM", "ymd HMS", "ymd"))
+    
+    expect_equal(class(res), c("POSIXct", "POSIXt"))
+    expect_equal(
+      res, as.POSIXct(c("2020-01-01 12:00:00", "2020-01-01 00:10:00", "2020-01-01 12:00:30",
+                        "2020-01-01 13:12:20", "2021-02-05 00:00:00", "2023-12-05 00:00:00"), tz = "UTC")
+    )
+  }
+)
+
+
+test_that(
+  "dates-times provided as POSIXct POSIXt input are returned correctly", {
+    
+    ## pre-processed with ymd_hms
+    date_time <- ymd_hms(c("2020-01-01 12:00:33", "2020-01-01 00:00:23", "2020-01-01 12:00:54", "2020-01-01 00:00:14", "2021-02-05 10:00:46"))
+    res <- convert_datetime(date_time, format = "ymd HMS")
+    expect_equal(class(res), c("POSIXct", "POSIXt"))
+    expect_equal(date_time, res)
+    
+    ## pre-processed with ymd_hm
+    date_time <- ymd_hm(c("2020-01-01 12:00", "2020-01-01 00:00", "2020-01-01 12:00", "2020-01-01 00:00", "2021-02-05 10:00"))
+    res <- convert_datetime(date_time, format = "ymd HM")
+    expect_equal(class(res), c("POSIXct", "POSIXt"))
+    expect_equal(date_time, res)
+    
+  }
+)
+
+test_that(
+  "date-only formats are returned correctly", {
+
+    ## dates pre-processed with ymd
+    date_time <- ymd(c("2020-01-01", "2020-01-01", "2020-01-01", "2020-01-01", "2021-02-05"))
+    res <- convert_datetime(date_time, format = "ymd")
+    expect_equal(class(res), c("POSIXct", "POSIXt"))
+    expect_equal(as.POSIXct(date_time, tz = "UTC"), res)
+    # should result in warning when expecting timestamp information
+    expect_warning(convert_datetime(date_time, format = c("ymd HM", "ymd HMS")))
+
+    ## dates pre-processed with as.Date input
+    date_time <- as.Date(c("2020-01-01 00:22", "2020-01-01 04:23", "2020-01-01 00:87:12", "2020-01-01", "2021-02-05"))
+    res <- convert_datetime(date_time, format = "ymd")
+    expect_equal(class(res), c("POSIXct", "POSIXt"))
+    expect_equal(as.POSIXct(date_time, tz = "UTC"), res)
+    # should result in warning when expecting timestamp information
+    expect_warning(convert_datetime(date_time, format = "ymd HM"))
+
+  }
+)
