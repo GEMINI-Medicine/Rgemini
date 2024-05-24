71a97f7418a0390769dc7c701269d7882655ab96
loffleraSMH <Anne.Loffler@unityhealth.to>
2024-05-24 18:35:23 -0400

    #76 trim ws

--- R/hospital_fiscal_year.R
+++ R/hospital_fiscal_year.R
@@ -10,14 +10,14 @@
 #' 2015-02-25, fiscal year will be 2014.
 #'
 #' @section Warning:
-#' NA values in returned vector indicates either missing date or inputs that
-#' cannot be converted into dates.
+#' NA values in returned vector indicates either missing date or its format is
+#' incorrect. All dates must be in `yyyy-mm-dd` format and precede the timestamp
+#' (e.g., `yyy-mm-dd hh:mm`).
 #'
-#' @param date_ymd (`character` | `POSIXct POSIXt`)\cr
-#' A vector of dates or date-times, typically in `yyyy-mm-dd` or
-#' `yyyy-mm-dd hh:mm` format, but other formats are acceptable as long as they
-#' can be interpreted by `lubridate::year()` and `lubridate::month()`.
-#' 
+#' @param date_ymd (`character`)\cr
+#' A character vector of dates or date-times.
+#' The date value must be in `yyyy-mm-dd` format.
+#'
 #' @return
 #' Numeric vector with the same number of entries as input, containing the
 #' derived fiscal year of all input dates (e.g., 2010, 2011, 2012, and so on).
@@ -28,14 +28,19 @@
 #' ipadmdad$fisc_year <- hospital_fiscal_year(ipadmdad$discharge_date_time)
 #' }
 #'
+#' @importFrom stringr str_sub
 #' @export
 hospital_fiscal_year <- function(date_ymd) {
+
+  # remove any leading/trailing " "
+  date_ymd <- trimws(date_ymd)
+
   # obtain year from date vector
-  yr <- lubridate::year(date_ymd)
+  yr <- as.numeric(str_sub(date_ymd, 1, 4))
 
   # based on month in date, assign fiscal year
   # (month < 4 belongs to previous fiscal year)
-  ifelse(lubridate::month(date_ymd) < 4,
-    yr - 1, yr
+  ifelse(as.numeric(str_sub(date_ymd, 6, 7)) < 4,
+         yr - 1, yr
   )
 }
