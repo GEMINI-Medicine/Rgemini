d39af4f9dfaa6626ca35c5dfb7d5c51d45b187af
Anne Loffler <Anne.Loffler@unityhealth.to>
2025-03-10 20:54:16 -0400

    update data reference

--- R/data.R
+++ R/data.R
@@ -1,55 +1,73 @@
+#' @title
 #' Dummy ipdiagnosis data
 #'
+#' @description
+#' This mimics the GEMINI ipdiagnosis table. And is generated with the `dummy_diag` function.
+#' See the [GEMINI Data Repository Dictionary](https://drive.google.com/uc?export=download&id=1iwrTz1YVz4GBPtaaS9tJtU0E9Bx1QSM5)
+#' for details.
+#'
+#' @keywords internal
 #' @name dummy_ipdiagnosis
 #' @docType data
+#'
 NULL
 
-
+#' @title
 #' Dummy daily census data
 #'
+#' @description
+#' Used to generate mock output for the `daily_census` vignette.
+#'
+#' @keywords internal
 #' @name dummy_census
 #' @docType data
+#'
 NULL
 
-
+#' @title
 #' Dummy CCSR data
 #'
+#' @description
+#' Used to generate mock output for the `icd_to_ccsr` vignette.
+#'
+#' @keywords internal
 #' @name dummy_ccsr
 #' @docType data
-NULL
-
-
-#' Mapping data for `frailty_score` function
 #'
-#' @name mapping_cihi_frailty
-#' @docType data
 NULL
 
-
-#' Mapping data for `disability` function
+#' @title
+#' Dummy coverage data
 #'
-#' @name mapping_disability
-#' @docType data
-NULL
-
-
-#' Dummy data for `data_coverage` function
-#' Coverage data
+#' @description
+#' Used to generate mock output for the `data_coverage` vignette.
+#'
+#' @keywords internal
 #' @name coverage
 #' @docType data
-#' @keywords internal
+#'
 NULL
 
-#' Dummy data for `data_coverage` function
-#' Coverage data
+#' @title
+#' Dummy coverage data (encounter-level)
+#'
+#' @description
+#' Used to generate mock output for the `data_coverage` vignette.
+#'
+#' @keywords internal
 #' @name coverage_enc
 #' @docType data
-#' @keywords internal
+#'
 NULL
 
-#' Dummy data for `data_coverage` function
-#' Coverage data
+#' @title
+#' Dummy coverage data (timeline)
+#'
+#' @description
+#' Used to generate mock output for the `data_coverage` vignette.
+#'
+#' @keywords internal
 #' @name coverage_timeline
 #' @docType data
-#' @keywords internal
+#'
 NULL
