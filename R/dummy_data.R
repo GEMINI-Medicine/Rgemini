22835a2813b8d175a228e1c332de7ed704c56b53
samantha-SMH <samantha-SMH@users.noreply.github.com>
2025-10-31 15:05:55 +0000

    Run styler & update documentation

--- R/dummy_data.R
+++ R/dummy_data.R
@@ -684,7 +684,7 @@ dummy_admdad <- function(id, admtime) {
 #'
 #' @import lubridate
 #' @export
-#' 
+#'
 #' @examples
 #' cohort <- dummy_ipadmdad()
 #' sample_scu_date_time(cohort, use_ip_dates = TRUE, seed = 1)
@@ -930,7 +930,7 @@ sample_scu_date_time <- function(scu_cohort, use_ip_dates = TRUE, start_date = N
 #'   - 95: Step-Down Surgical Unit
 #'
 #' @export
-#' 
+#'
 #' @examples
 #' dummy_ipscu(nid = 100, n_hospitals = 10, time_period = c(2015, 2023))
 #' dummy_ipscu(nid = 11, n_hospitals = 1, time_period = c("2020-01-01", "2021-01-01"))
@@ -1107,10 +1107,10 @@ dummy_ipscu <- function(nid = 1000, n_hospitals = 10, time_period = c(2015, 2023
 #' - `genc_id` (`integer`): GEMINI encounter ID
 #' - `hospital_num` (`integer`): Hospital ID
 #' - `triage_date_time` (`character`): The date and time of triage
-#' 
+#'
 #' @import lubridate
 #' @export
-#' 
+#'
 #' @examples
 #' cohort <- dummy_ipadmdad()
 #' dummy_er(cohort = cohort, seed = 1)
