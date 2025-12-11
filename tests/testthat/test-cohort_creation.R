5986c36fdde0a4a1d60d9c0763309c2c39de9fd6
loffleraSMH <Anne.Loffler@unityhealth.to>
2025-12-11 12:46:18 -0500

    #247 revert unit tests

--- tests/testthat/test-cohort_creation.R
+++ tests/testthat/test-cohort_creation.R
@@ -1,6 +1,6 @@
 test_that("cohort_creation inclusions/exclusions are applied correctly", {
   set.seed(1)
-  dummy_data <- dummy_ipadmdad(300, n_hospitals = 2, seed = 1) %>%
+  dummy_data <- Rgemini::dummy_ipadmdad(300, n_hospitals = 2) %>%
     data.table()
 
   cohort <- cohort_creation(
@@ -19,15 +19,15 @@ test_that("cohort_creation inclusions/exclusions are applied correctly", {
     exclusion_flag = c(FALSE, FALSE, FALSE, TRUE)
   )
 
-  expect_equal(nrow(cohort[[1]]), 83)
+  expect_equal(nrow(cohort[[1]]), 96)
 
-  expected_output <- data.table(`N (%)` = c("300", "150 (50%)", "95 (63.3%)", "-12 (-12.6%)", "83"))
+  expected_output <- data.table(`N (%)` = c("300", "156 (52%)", "103 (66%)", "-7 (-6.8%)", "96"))
   expect_equal(cohort[[2]][, 3], expected_output)
 })
 
 test_that("grouping works as expected", {
   set.seed(1)
-  dummy_data <- dummy_ipadmdad(300, n_hospitals = 2, seed = 1) %>%
+  dummy_data <- Rgemini::dummy_ipadmdad(300, n_hospitals = 2) %>%
     data.table()
 
   # apply grouping by hospital_num
@@ -48,12 +48,12 @@ test_that("grouping works as expected", {
     group_var = "hospital_num"
   )
 
-  expect_equal(nrow(cohort[[1]]), 51)
+  expect_equal(nrow(cohort[[1]]), 49)
 
   expected_output <- data.table(
-    `Overall N (%)` = c("300", "150 (50%)", "-95 (-63.3%)", "51 (92.7%)"),
-    `1` = c("155", "75 (48.4%)", "-47 (-62.7%)", "26 (92.9%)"),
-    `2` = c("145", "75 (51.7%)", "-48 (-64%)", "25 (92.6%)")
+    `Overall N (%)` = c("300", "156 (52%)", "-103 (-66%)", "49 (92.5%)"),
+    `1` = c("155", "77 (49.7%)", "-41 (-53.2%)", "34 (94.4%)"),
+    `2` = c("145", "79 (54.5%)", "-62 (-78.5%)", "15 (88.2%)")
   )
   expect_equal(cohort[[2]][, 3:5], expected_output)
 })
@@ -61,7 +61,7 @@ test_that("grouping works as expected", {
 
 test_that("cell suppression works as expected", {
   set.seed(1)
-  dummy_data <- dummy_ipadmdad(300, n_hospitals = 2, seed = 1) %>%
+  dummy_data <- Rgemini::dummy_ipadmdad(300, n_hospitals = 2) %>%
     data.table()
 
   expect_warning( # should produce warning
@@ -85,14 +85,14 @@ test_that("cell suppression works as expected", {
   )
 
   expected_output <- data.table(`N (%)` = c(
-    "300", "150 (50%)", "-124 (-82.7%)", "15 (57.7%)", "N < 6", "13"
+    "300", "156 (52%)", "-135 (-86.5%)", "12 (57.1%)", "N < 6", "11"
   ))
   expect_equal(cohort[[2]][, 3], expected_output)
 })
 
 test_that("show_prct works as expected", {
   set.seed(1)
-  dummy_data <- dummy_ipadmdad(300, n_hospitals = 2, seed = 1) %>%
+  dummy_data <- Rgemini::dummy_ipadmdad(300, n_hospitals = 2) %>%
     data.table()
 
   cohort <- cohort_creation(
@@ -113,7 +113,7 @@ test_that("show_prct works as expected", {
   )
 
   expected_output <- data.table(N = c(
-    "300", "150", "95", "-12", "83"
+    "300", "156", "103", "-7", "96"
   ))
   expect_equal(cohort[[2]][, 3], expected_output)
 })
