fbbf8a5bba7a37f35dcad1b5a9db20a5c17a23a6
Anne Loffler <Anne.Loffler@unityhealth.to>
2026-01-26 19:24:55 -0500

    #24 lint file

--- R/cci_group.R
+++ R/cci_group.R
@@ -8,7 +8,7 @@
 #'
 #' The `cci_group()` function categorizes CCI codes into broader, clinically
 #' meaningful intervention categories based on each code's first 3 characters.
-#' 
+#'
 #' Broadly, intervention codes can be grouped into the following CCI Sections
 #' based on the first digit:
 #' - 1: Physical/Physiological Therapeutic Interventions (1AA - 1ZZ)
@@ -26,10 +26,10 @@
 #' - Therapeutic Interventions on the Eye and Ocular Adnexa (1CC - 1CZ)
 #' - Therapeutic Interventions on the Ear and Mastoid (1DA - 1DZ)
 #' - etc.
-#' 
+#'
 #' For a complete list of all subsections, see
 #' [CIHI CCI sections and code ranges](https://www.cihi.ca/en/overview-of-cci-sections-and-code-ranges).
-#' 
+#'
 #' @param cci_codes (`data.frame` | `data.table`)
 #' Table containing `intervention_code` column that lists all CCI intervention
 #' codes of interest (e.g., from the `ipintervention` or `erintervention` table).
@@ -47,12 +47,12 @@
 #'     "1MA52HA", "2PB70DA", "3SC10KM", "5LB08ZZ", "6KA02ME", "8AA70BABA"
 #'     ))
 #' res <- cci_group(cci_codes)
-#' 
+#'
 #' @references
 #' https://www.cihi.ca/en/overview-of-cci-sections-and-code-ranges
-#' 
+#'
 #' @export
-#' 
+#'
 cci_group <- function(cci_codes) {
 
   ## prepare data
@@ -73,7 +73,7 @@ cci_group <- function(cci_codes) {
   mapping_cci[] <- lapply(mapping_cci, function(x) {
     if (is.character(x)) trimws(x) else x
   })
-  
+
   ## add CCI section (based on first number)
   cci_grouped[, section := as.numeric(substr(intervention_code, 1, 1))]
   cci_grouped[, section := ifelse(section %in% c(1, 2, 3, 5, 6, 7, 8), section, NA)]
@@ -83,7 +83,7 @@ cci_group <- function(cci_codes) {
     by.x = "section", by.y = "idx", all.x = TRUE, sort = FALSE
   )
   setnames(cci_grouped, "description", "section_descr")
-  
+
   ## add CCI subsection (based on first 3 characters)
   cci_grouped[, subsection := substr(intervention_code, 1, 3)]
   mapping_cci[field == "Group", subsection := paste0(section, idx)]
@@ -92,12 +92,12 @@ cci_group <- function(cci_codes) {
     mapping_cci[field == "Group", c("subsection", "subsection_descr")],
     by = "subsection", all.x = TRUE, sort = FALSE
   )
-  
+
   # clean up final output
   cci_grouped <- cci_grouped %>%
     select(intervention_code, section, section_descr, subsection, subsection_descr, everything()) %>%
     data.table()
   cci_grouped[cci_grouped == ""] <- NA
-  
+
   return(cci_grouped)
 }
