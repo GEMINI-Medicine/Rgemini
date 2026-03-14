74e57a31d66dddf5b64cd5fabee86227b21559dd
Anne Loffler <Anne.Loffler@unityhealth.to>
2026-03-14 09:41:42 -0400

    #266 fix dbcon unit test

--- tests/testthat/test-check_input.R
+++ tests/testthat/test-check_input.R
@@ -13,7 +13,15 @@ test_that("correct inputs pass checks", {
                                     genc_id = as.integer(5),
                                     discharge_date_time = "2020-01-01 00:00"
                                   ),
-                                  input7 = dbDriver("PostgreSQL"), # DB connection
+                                  input7 = dbConnect(
+                                    RPostgres::Postgres(),
+                                    host = "gemini-db-dummy.j.aivencloud.com",
+                                    port = 10571,
+                                    dbname = "dummy_db_v1_0_0", # most recent version
+                                    user = "gemini_user",
+                                    password = "gemini",
+                                    sslmode = "require"
+                                  ), # DB connection
                                   input8 = list(1, 2) # list inputs
   ) {
     expect_no_error(
