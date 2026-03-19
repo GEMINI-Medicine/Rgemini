d739e2776247b5701126ec6498dd2815c4c9ac2d
loffleraSMH <loffleraSMH@users.noreply.github.com>
2026-03-19 22:08:09 +0000

    Run styler & update documentation

--- R/utils.R
+++ R/utils.R
@@ -492,14 +492,14 @@ check_input <- function(arginput, argtype,
 
       ## For all other inputs
     } else if ((any(argtype == "integer") && !all(is_integer(arginput))) ||
-               (!any(argtype == "integer") && !any(class(arginput) %in% argtype) &&
-                (!(any(argtype == "numeric") &&
-                   all(is_integer(arginput)))))) { # in case argtype is "numeric" and provided input is "integer", don't show error
+      (!any(argtype == "integer") && !any(class(arginput) %in% argtype) &&
+        (!(any(argtype == "numeric") &&
+          all(is_integer(arginput)))))) { # in case argtype is "numeric" and provided input is "integer", don't show error
       stop(
         paste0(
           "Invalid user input in '", as.character(sys.calls()[[1]])[1], "': '",
           argname, "' needs to be of type '", paste(argtype,
-                                                    collapse = "' or '"
+            collapse = "' or '"
           ), "'.",
           "\nPlease refer to the function documentation for more details."
         ),
@@ -598,8 +598,8 @@ check_input <- function(arginput, argtype,
       # ignore coltypes without specification ("")
       check_col_type <- function(col, coltype) {
         if (coltype != "" && !any(grepl(coltype,
-                                        class(as.data.table(arginput)[[col]]),
-                                        ignore.case = TRUE
+          class(as.data.table(arginput)[[col]]),
+          ignore.case = TRUE
         ))) {
           stop(
             paste0(
@@ -882,13 +882,13 @@ convert_dt <- function(dt_var,
     if (is.null(addtl_msg) || !addtl_msg %in% c("", " ", "\n")) {
       warning(
         ifelse(is.null(addtl_msg),
-               paste0(
-                 "Please carefully consider how to deal with missing/invalid date-time",
-                 " entries and perform any additional pre-processing prior to running",
-                 " the function `", as.character(sys.calls()[[1]])[1],
-                 "` (e.g., impute missing dates/timestamps etc.).\n"
-               ),
-               addtl_msg
+          paste0(
+            "Please carefully consider how to deal with missing/invalid date-time",
+            " entries and perform any additional pre-processing prior to running",
+            " the function `", as.character(sys.calls()[[1]])[1],
+            "` (e.g., impute missing dates/timestamps etc.).\n"
+          ),
+          addtl_msg
         ),
         immediate. = TRUE, call. = FALSE
       )
